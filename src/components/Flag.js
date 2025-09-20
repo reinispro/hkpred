1160f5a45b88b5ad71739459a573a6ddb0b5777a
reinispro <12019050+reinispro@users.noreply.github.com>
2025-09-21 00:29:21 +0300

    .

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -6,7 +6,7 @@ const Flag = ({ country, size = 24 }) => {
   if (!code) {
     return (
       <span className="inline-block w-8 h-6 bg-gray-700 text-xs text-white flex items-center justify-center">
-        {"?"}
+        ?
       </span>
     );
   }
