8ba0971a5230dda0dfb2e7f52de8ab11ca035069
reinispro <12019050+reinispro@users.noreply.github.com>
2025-09-21 00:31:32 +0300

    .

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -3,20 +3,25 @@ import countryCodes from "@/lib/countryCodes";
 
 const Flag = ({ country, size = 24 }) => {
   const code = countryCodes[country];
+  
   if (!code) {
+    const fallbackContent = "?";
     return (
       <span className="inline-block w-8 h-6 bg-gray-700 text-xs text-white flex items-center justify-center">
-        {"?"}
+        {fallbackContent}
       </span>
     );
   }
+  
+  const flagHeight = (size * 3) / 4;
+  
   return (
     <img
       src={`https://flagcdn.com/w${size}/${code}.png`}
       alt={country}
       className="inline-block rounded-sm"
       width={size}
-      height={(size * 3) / 4} // karogu proporcija 4:3
+      height={flagHeight}
     />
   );
 };
